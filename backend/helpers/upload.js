c2d37ee62b794591b892d9e710b0bbf394768dd4
celoaraujo <celoparaujo99@gmail.com>
2024-06-11 17:58:43 -0300

    configurando upload

---
+++ backend/helpers/upload.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+
+const path = require("path");
+
+const storage = multer.diskStorage({
+  // cb = callback
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/images/"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); //teste.jpg -> extname(algumacoisa.jpg)
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+module.exports = upload
