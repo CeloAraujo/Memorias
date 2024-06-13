0581998325664cb73530f122fa3de4200bf75e48
celoaraujo <celoparaujo99@gmail.com>
2024-06-13 17:27:37 -0300

    axios e adicionar memoria (template)

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
+import "./AddMemory.css";
 const AddMemory = () => {
   return (
-    <div>AddMemory</div>
-  )
-}
+    <div className="addmemory-page">
+      <h2>Crie sua nova memória</h2>
+      <form>
+        <label>
+          <p>Título</p>
+          <input
+            type="text"
+            placeholder="Define seu título"
+            name="title"
+            required
+          />
+        </label>
+        <label>
+          <p>Descrição</p>
+          <textarea
+            placeholder="Fale sobre sua memória"
+            name="description"
+            required
+          ></textarea>
+        </label>
+        <label>
+          <p>Imagem:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" className="btn" />
+      </form>
+    </div>
+  );
+};
 
-export default AddMemory
+export default AddMemory;
