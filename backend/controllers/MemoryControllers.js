425a025d40a4d213d0199c18bf0f25ba16768965
celoaraujo <celoparaujo99@gmail.com>
2024-06-13 14:38:58 -0300

    toggle favorite memory

--- backend/controllers/MemoryControllers.js
+++ backend/controllers/MemoryControllers.js
@@ -115,10 +115,28 @@ const updateMemory = async (req, res) => {
   }
 };
 
+const toggleFavorite = async (req, res) => {
+  try {
+    const memory = await Memory.findById(req.params.id);
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada" });
+    }
+    memory.favorite = !memory.favorite;
+
+    await memory.save();
+
+    res.json({ msg: "Adicionado aos favoritos!", memory });
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro");
+  }
+};
+
 module.exports = {
   createMemory,
   getMemories,
   getMemory,
   deleteMemory,
   updateMemory,
+  toggleFavorite,
 };
--- backend/routes.js
+++ backend/routes.js
@@ -10,6 +10,7 @@ const {
   getMemory,
   deleteMemory,
   updateMemory,
+  toggleFavorite,
 } = require("./controllers/MemoryControllers");
 
 router.post(
@@ -38,5 +39,7 @@ router.delete("/:id", (req, res) => deleteMemory(req, res));
 router.patch("/:id", upload.single("image"), (req, res) =>
   updateMemory(req, res)
 );
+
+router.patch("/favorite/:id", (req, res) => toggleFavorite(req, res));
 
 module.exports = router;
