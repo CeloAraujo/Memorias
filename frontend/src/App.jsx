0e78d683c186aafa844fc5456ba6239a990d0ad5
celoaraujo <celoparaujo99@gmail.com>
2024-06-13 15:31:47 -0300

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>memories</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
