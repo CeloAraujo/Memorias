14f2e2862a9f38cc76189b8ac1e71de1fab79b09
celoaraujo <celoparaujo99@gmail.com>
2024-06-17 10:24:39 -0300

    update baseURL with backend deploy

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://memorias-i0e9.onrender.com/memories";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
